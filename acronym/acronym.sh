e5aa3de71e5c43f7747f856164c1a7882b13b372
lemoncurry <git@annakrueger.de>
2019-02-14 11:31:04 +0100

    acronym: improve solution, input processing

--- acronym/acronym.sh
+++ acronym/acronym.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-  echo 'Es darf nur genau ein Paramter angegeben werden.'
-  exit 1
-fi
+phrase=${1?:'Es darf nur genau ein Parameter angegeben werden'}
 
-IFS=' |-' read -r -a words <<< "$1"
+IFS=' |-' read -r -a words <<< "$phrase"
 
 for word in "${words[@]}"; do
   firstCharacter="${word:0:1}"
@@ -13,5 +10,4 @@ for word in "${words[@]}"; do
 done
 
 echo "$acronym"
-
 exit 0
